415963601b66562ac7a8ef44f04671fa9e08c676
OMAR BEEN HASIB <omarbeenhasib@OMARs-MacBook-Air.local>
2024-10-17 16:23:20 -0700

    Commiting refactored codes using modularization and dry method of Rock Paper Scissor game script.

--- rockPaperScissorGame.py
+++ rockPaperScissorGame.py
@@ -1,29 +1,45 @@
 import random
 
-choices=("r","p","s")
-emojis={"r":"🪨","p":"📜","s":"✂️"}
+ROCK="r"
+PAPER="p"
+SCISSOR="s"
 
-while True:
-    user_choice=input("Rock, Paper, Scissors? : type r or p or s").lower()
-    if user_choice not in choices:
-        print("Invalid input, input type r or p or s")
-        continue
+emojis={ROCK:"🪨",PAPER:"📜",SCISSOR:"✂️"}
+choices= tuple(emojis.keys())
+def get_user_choice():
+    while True:
+        user_choice = input("Rock, Paper, Scissors? : type r or p or s").lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid input, input type r or p or s")
 
-    comp_choice=random.choice(choices)
-
+def print_choices(user_choice, comp_choice):
     print(f"You chose {emojis[user_choice]}")
     print(f"Computer chose {emojis[comp_choice]}")
 
+def determine_result(user_choice, comp_choice):
     if user_choice == comp_choice:
         print("Game Tied!! Try again!!")
-    elif ((user_choice=="r" and comp_choice=="s") or
-        (user_choice=="s"  and comp_choice== "p") or
-        (user_choice== "p" and comp_choice== "r")):
+    elif ((user_choice==ROCK and comp_choice==SCISSOR) or
+          (user_choice==SCISSOR  and comp_choice== PAPER) or
+          (user_choice== PAPER and comp_choice== ROCK)):
         print("Congratulations!! You win ...")
     else:
         print("You lose ...")
 
-    continue_game=input("Do you want to play again? (y/n) : ").lower()
-    if continue_game=="n":
-        break
+def play():
+    while True:
+        user_choice = get_user_choice()
+        comp_choice=random.choice(choices)
+        print_choices(user_choice, comp_choice)
+        determine_result(user_choice, comp_choice)
 
+        continue_game=input("Do you want to play again? (y/n) : ").lower()
+        if continue_game=="n":
+            break
+
+
+
+
+play()
