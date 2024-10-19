cfe0c19a1da94fb296d10c879ca8b0f276c98e58
OMAR BEEN HASIB <adonomar02@gmail.com>
2024-10-19 12:47:03 -0700

    Adding currency converter refactored file...

--- currency_converter.py
+++ currency_converter.py
@@ -1,43 +1,43 @@
-from math import trunc
+def get_amount():
+    while True:
+        try:
+            amount = float(input("Enter amount to convert: "))
+            if amount <= 0:
+                raise ValueError()
+            return amount
+        except ValueError:
+            print("Please enter a valid number.")
 
-while True:
-    try:
-        amount = float(input("Enter amount to convert: "))
-        if amount <= 0:
-            raise ValueError()
-        break
-    except ValueError:
-        print("Please enter a valid number.")
 
-
-currency = {
+exchange_rates = {
     "USD":{"EUR":.90, "CAD":1.10},
     "EUR":{"USD":1.1, "CAD":1.30},
     "CAD":{"EUR":.68, "USD":.80}
 }
 
-while True:
-    try:
-        source_cur=input("Enter source currency to convert (USD/EUR/CAD): ").upper()
-        if source_cur not in currency.keys():
-            raise ValueError()
-        break
-    except ValueError:
-        print("Please enter a valid currency.")
+def get_currency(label):
+    while True:
+        try:
+            currency=input(f"Enter {label} currency to convert (USD/EUR/CAD): ").upper()
+            if currency not in exchange_rates.keys():
+                raise ValueError()
+            return currency
+        except ValueError:
+            print("Please enter a valid currency.")
 
-while True:
-    try:
-        target_cur=input("Enter target currency to convert (USD/EUR/CAD): ").upper()
-        if source_cur not in currency.keys():
-            raise ValueError()
-        break
-    except ValueError:
-        print("Please enter a valid currency.")
+def calculate_currency(amount, source_cur, target_cur):
+    if source_cur==target_cur:
+        return amount
 
-if source_cur==target_cur:
-    calculation=amount
-else:
-    calculation= amount*currency[source_cur][target_cur]
-                     
-print(f"Your {source_cur} {amount} equals to  {target_cur} {calculation:.2f}")
+    return amount * exchange_rates[source_cur][target_cur]
 
+
+def main():
+    amount = get_amount()
+    source_cur=get_currency("Source")
+    target_cur=get_currency("Target")
+    converted_amount=calculate_currency(amount, source_cur, target_cur)
+    print(f"converted currency:  {source_cur} {amount} equals to  {target_cur} {converted_amount}")
+
+if __name__ == "__main__":
+    main()
