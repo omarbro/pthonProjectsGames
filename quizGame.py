6093f43545aa69d2d6698e380b311026d34fd706
OMAR BEEN HASIB <adonomar02@gmail.com>
2024-10-21 09:15:04 -0700

    refactored file of quizGame

--- quizGame.py
+++ quizGame.py
@@ -1,40 +1,52 @@
 import random
 from termcolor import cprint
 
-quiz=[
-    {
-        'question':'What is the capital of France?',
-        'options':['A. NICE', 'B. LYON', 'C.DHAKA', 'D.PARIS'],
-        'answer': 'D'
-    },
-    {
-        'question':'What is the capital of Finland?',
-        'options':['A. NICE' , 'B. LYON', 'C.HELSINKI', 'D.PARIS'],
-        'answer': 'C'
-    },
-    {
-        'question':'What is the capital of NORWAY?',
-        'options':['A. NICE', 'B. OSLO', 'C.DHAKA', 'D.PARIS'],
-        'answer': 'B'
-    }
-]
 
-random.shuffle(quiz)
-score=0
 
-for index, item in enumerate(quiz, 1):
-    print(f'{index}: {item["question"]}')
-    for option in item['options']:
+
+
+def ask_question(index, question, options):
+    print(f'{index}: {question}')
+    for option in options:
         cprint(f'{option}', 'blue')
 
-    answer = input("Give you answer (A/B/C/D: )").upper().strip()
-    if answer == item['answer']:
-        cprint("Correct!", 'green' )
-        score=score+1
-    else:
-        print(f"Sorry, that's not correct.Correct answer is : {item['answer']}", 'red')
+    return input("Give you answer (A/B/C/D: )").upper().strip()
+
+def run_quiz(quiz):
+    score=0
+    random.shuffle(quiz)
+    for index, item in enumerate(quiz, 1):
+        answer=ask_question(index, item["question"], item["options"])
+        if answer == item['answer']:
+            cprint("Correct!", 'green' )
+            score=score+1
+        else:
+           cprint(f"Sorry, that's not correct. Correct answer is : {item['answer']}", 'red')
+        print("\n")
+    print(f"Your final score is {score}/{len(quiz)}")
 
 
-print(f"Your final score is {score}/{len(quiz)}")
+def main():
+    quiz=[
+        {
+            'question':'What is the capital of France?',
+            'options':['A. NICE', 'B. LYON', 'C.DHAKA', 'D.PARIS'],
+            'answer': 'D'
+        },
+        {
+            'question':'What is the capital of Finland?',
+            'options':['A. NICE' , 'B. LYON', 'C.HELSINKI', 'D.PARIS'],
+            'answer': 'C'
+        },
+        {
+            'question':'What is the capital of NORWAY?',
+            'options':['A. NICE', 'B. OSLO', 'C.DHAKA', 'D.PARIS'],
+            'answer': 'B'
+        }
+    ]
+
+    run_quiz(quiz)
 
 
+if __name__ == '__main__':
+    main()
